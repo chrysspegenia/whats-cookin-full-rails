559195753ea4144d1e62603df35b011fc003e976
GARS <christian.garcia4297@gmail.com>
2024-05-25 12:47:24 +0800

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # resources :recipes
   resources :edamam
 
+  resources :meal_planner, only: [:index]
+
   # get "search" => "edamam#search"
 
   resources :spoonacular, only: [:index]
