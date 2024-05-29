24d6b9267cae580e93bbb5686b3a595366aa2a4a
Marc Dimacuha <66410761+dimacuhamarc@users.noreply.github.com>
2024-05-29 17:55:06 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_051337) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
->>>>>>> origin/main
   end
 
   create_table "users", force: :cascade do |t|
