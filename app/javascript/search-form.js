12ee9ff2bc4760d10884d5977b91e05e98d2e460
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-05-31 23:37:31 +0800

    Add transition effects for ingredients wrapper

--- app/javascript/search-form.js
+++ app/javascript/search-form.js
@@ -43,4 +43,37 @@ document.addEventListener("turbo:load", function () {
 
   // Initial check to set the button text correctly
   updateButtonText();
+
+  const displayIngredientsBtn = document.querySelector(
+    ".display-ingredients-btn"
+  );
+
+  const ingredientsSearchWrapper = document.querySelector(
+    ".ingredients-search-wrapper"
+  );
+
+  const searchSubSection = document.querySelector(".search-sub-section");
+
+  function updateDisplayIngredients(wrapperElement, storageKey) {
+    if (wrapperElement) {
+      let isDisplayed = localStorage.getItem(storageKey) === "true";
+      if (isDisplayed) {
+        wrapperElement.classList.add("display");
+      }
+    }
+
+    displayIngredientsBtn.addEventListener("click", function () {
+      wrapperElement.classList.toggle("display");
+      localStorage.setItem(
+        storageKey,
+        wrapperElement.classList.contains("display")
+      );
+    });
+  }
+
+  updateDisplayIngredients(
+    ingredientsSearchWrapper,
+    "displayIngredientsWrapper"
+  );
+  updateDisplayIngredients(searchSubSection, "displaySubSection");
 });
