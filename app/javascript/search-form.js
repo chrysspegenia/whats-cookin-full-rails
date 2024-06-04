5c4a742fbcdbe58405e15ae2a694bf84bf116ffd
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-06-04 19:53:28 +0800

    Add element check before applying eventlisteners on page load

--- app/javascript/search-form.js
+++ app/javascript/search-form.js
@@ -1,6 +1,9 @@
 document.addEventListener("turbo:load", function () {
   function handleCheckboxes(selector) {
-    document.querySelectorAll(selector).forEach((checkbox) => {
+    const checkboxes = document.querySelectorAll(selector);
+    if (checkboxes.length === 0) return; // Exit if no checkboxes found
+
+    checkboxes.forEach((checkbox) => {
       if (checkbox.checked) {
         checkbox.parentElement.classList.add("checked");
       }
@@ -15,34 +18,36 @@ document.addEventListener("turbo:load", function () {
 
   const checkAllBtn = document.querySelector(".check-all-btn");
 
-  function updateButtonText() {
-    const ingredientCheckboxes = document.querySelectorAll(
-      ".ingredient-checkbox"
-    );
-    const allChecked = Array.from(ingredientCheckboxes).every(
-      (checkbox) => checkbox.checked
-    );
-    checkAllBtn.textContent = allChecked
-      ? "Unselect All Ingredients"
-      : "Select All Ingredients";
-  }
+  if (checkAllBtn) {
+    function updateButtonText() {
+      const ingredientCheckboxes = document.querySelectorAll(
+        ".ingredient-checkbox"
+      );
+      const allChecked = Array.from(ingredientCheckboxes).every(
+        (checkbox) => checkbox.checked
+      );
+      checkAllBtn.textContent = allChecked
+        ? "Unselect All Ingredients"
+        : "Select All Ingredients";
+    }
 
-  checkAllBtn.addEventListener("click", function () {
-    const checkboxes = document.querySelectorAll(".ingredient-checkbox");
-    const allChecked = Array.from(checkboxes).every(
-      (checkbox) => checkbox.checked
-    );
+    checkAllBtn.addEventListener("click", function () {
+      const checkboxes = document.querySelectorAll(".ingredient-checkbox");
+      const allChecked = Array.from(checkboxes).every(
+        (checkbox) => checkbox.checked
+      );
 
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = !allChecked;
-      checkbox.dispatchEvent(new Event("change")); // trigger change event to update parent class
+      checkboxes.forEach((checkbox) => {
+        checkbox.checked = !allChecked;
+        checkbox.dispatchEvent(new Event("change")); // trigger change event to update parent class
+      });
+
+      updateButtonText();
     });
 
+    // Initial check to set the button text correctly
     updateButtonText();
-  });
-
-  // Initial check to set the button text correctly
-  updateButtonText();
+  }
 
   const displayIngredientsBtn = document.querySelector(
     ".display-ingredients-btn"
@@ -61,26 +66,28 @@ document.addEventListener("turbo:load", function () {
         wrapperElement.classList.add("display");
         displayIngredientsBtn.classList.add("bxs-chevron-up");
       }
+
+      displayIngredientsBtn.addEventListener("click", function () {
+        wrapperElement.classList.toggle("display");
+        localStorage.setItem(
+          storageKey,
+          wrapperElement.classList.contains("display")
+        );
+
+        if (wrapperElement.classList.contains("display")) {
+          displayIngredientsBtn.classList.add("bxs-chevron-up");
+        } else {
+          displayIngredientsBtn.classList.remove("bxs-chevron-up");
+        }
+      });
     }
-
-    displayIngredientsBtn.addEventListener("click", function () {
-      wrapperElement.classList.toggle("display");
-      localStorage.setItem(
-        storageKey,
-        wrapperElement.classList.contains("display")
-      );
-
-      if (wrapperElement.classList.contains("display")) {
-        displayIngredientsBtn.classList.add("bxs-chevron-up");
-      } else {
-        displayIngredientsBtn.classList.remove("bxs-chevron-up");
-      }
-    });
   }
 
-  updateDisplayIngredients(
-    ingredientsSearchWrapper,
-    "displayIngredientsWrapper"
-  );
-  updateDisplayIngredients(searchSubSection, "displaySubSection");
+  if (displayIngredientsBtn) {
+    updateDisplayIngredients(
+      ingredientsSearchWrapper,
+      "displayIngredientsWrapper"
+    );
+    updateDisplayIngredients(searchSubSection, "displaySubSection");
+  }
 });
