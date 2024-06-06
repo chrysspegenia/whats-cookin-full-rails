4e1c179f3d767837d394deb3ea11ba3373fbdc8b
GARS <christian.garcia4297@gmail.com>
2024-06-06 21:25:21 +0800

    Modify meal_planner helper

--- app/helpers/meal_planner_helper.rb
+++ app/helpers/meal_planner_helper.rb
@@ -2,8 +2,17 @@ module MealPlannerHelper
     private
   
     def recipe_options
-      current_user.recipes.map { |recipe| [recipe.title, recipe.id] }
+      current_user.recipes.map do |recipe|
+        {
+          id: recipe.id,
+          title: recipe.title,
+          image_url: recipe.image_url,
+          meal_type: recipe.meal_type,
+          cuisine_type: recipe.cuisine_type
+        }
+      end
     end
+    
     
   end
   
