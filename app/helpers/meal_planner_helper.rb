b16b15e1a45c06883a7ab6b8d716caf00dc97ca8
GARS <christian.garcia4297@gmail.com>
2024-06-06 15:30:50 +0800

    Modify meal_planner_helper

--- app/helpers/meal_planner_helper.rb
+++ app/helpers/meal_planner_helper.rb
@@ -1,16 +1,9 @@
 module MealPlannerHelper
-    def meal_select_options(is_today)
-      content_tag(:div) do
-        3.times.map do
-          select_tag("", options_for_select(recipe_options, selected: ""), include_blank: true, class: is_today ? 'dropdown' : 'colored-dropdown')
-        end.join.html_safe
-      end
-    end
-  
     private
   
     def recipe_options
-      @recipes.map { |recipe| [recipe.title, recipe.id] }
+      current_user.recipes.map { |recipe| [recipe.title, recipe.id] }
     end
+    
   end
   
