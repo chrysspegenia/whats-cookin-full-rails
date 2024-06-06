5d7e28547508e8ed5bfcddc9672b6dea0f5e0a19
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-06-06 22:18:29 +0800

    Modify redirect path from dashboard to myrecipes

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,7 +1,7 @@
 class RedirectController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to dashboard_index_path
+      redirect_to myrecipes_path
     else
       redirect_to new_user_session_path
     end
