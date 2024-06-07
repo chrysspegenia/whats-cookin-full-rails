c01a425759657efc423355c5364493297a790d24
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-06-07 14:23:36 +0800

    Modify js file route

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,13 +1,11 @@
-import { Application } from "@hotwired/stimulus"
-import HelloController from "./hello_controller.js"
-import DragAndDropController from "./drag_and_drop_controller.js"
+import { Application } from "@hotwired/stimulus";
+import DragAndDropController from "./drag_and_drop_controller.js";
 
-const application = Application.start()
+const application = Application.start();
 
-application.register("hello", HelloController)
-application.register("drag-and-drop", DragAndDropController)
+application.register("drag-and-drop", DragAndDropController);
 
-application.debug = false
-window.Stimulus = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
