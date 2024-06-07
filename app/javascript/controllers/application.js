04db6b848bc5094f391f35b6748c1839a44c7a13
GARS <christian.garcia4297@gmail.com>
2024-06-07 14:12:20 +0800

    modify application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import { DragAndDropController } from "./drag_and_drop_controller"
 
 const application = Application.start()
+application.register("drag-and-drop", DragAndDropController)
 
 // Configure Stimulus development experience
 application.debug = false
