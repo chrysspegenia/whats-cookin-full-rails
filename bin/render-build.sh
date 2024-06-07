cb905072791b5e2a5e2b250a8d925b773fd67a68
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-06-07 17:35:22 +0800

    Set up assets compiler

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+RAILS_ENV=production bundle install
+RAILS_ENV=production bundle exec rails assets:precompile
+RAILS_ENV=production bundle exec rails assets:clean
+RAILS_ENV=production bundle exec rails db:migrate
